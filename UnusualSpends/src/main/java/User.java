cb398e36bd283beb61b1ff4f3edccfa8c78ead44
Sachin <Sachinkailasborse@gmail.com>
2024-03-11 22:55:02 +0530

    Make it pass

--- UnusualSpends/src/main/java/User.java
+++ UnusualSpends/src/main/java/User.java
@@ -1,11 +1,23 @@
 
 
 public class User {
-     private String name;
+    private String name;
+    private String emailId;
     
 
     public User(String name, String emailId) {
         this.name = name;
-        
+        this.emailId = emailId;
+       
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+   
 }
