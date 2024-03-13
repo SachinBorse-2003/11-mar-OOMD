23b85e34fffadc0a190dca9140963cbc122c12f6
Sachin <Sachinkailasborse@gmail.com>
2024-03-13 22:25:44 +0530

    Add regex and change nomenclature of tests

--- UnusualSpends/src/main/java/User.java
+++ UnusualSpends/src/main/java/User.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class User {
     private String name;
@@ -7,12 +8,8 @@ public class User {
     private List<Transaction> transactions;
 
     public User(String name, String emailId) {
-        if (name == null || name.isEmpty()) {
-            throw new IllegalArgumentException("Name cannot be null or empty");
-        }
-        if (emailId == null || emailId.isEmpty()) {
-            throw new IllegalArgumentException("Email ID cannot be null or empty");
-        }
+        validateName(name);
+        validateEmail(emailId);
         this.name = name;
         this.emailId = emailId;
         this.transactions = new ArrayList<>();
@@ -29,4 +26,24 @@ public class User {
     public List<Transaction> getTransactions() {
         return transactions;
     }
+
+    private void validateName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+        if (!Pattern.matches("^[a-zA-Z]+$", name)) {
+            throw new IllegalArgumentException("Name can only contain alphabets");
+        }
+    }
+
+    private void validateEmail(String emailId) {
+        if (emailId == null || emailId.isEmpty()) {
+            throw new IllegalArgumentException("Email ID cannot be null or empty");
+        }
+        if (!Pattern.matches("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$",emailId)) {
+            throw new IllegalArgumentException("Invalid email address format");
+        }
+    }
+
+
 }
--- UnusualSpends/src/test/java/UserTest.java
+++ UnusualSpends/src/test/java/UserTest.java
@@ -3,31 +3,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class UserTest {
-    @Test
-    public void testUserProperties() {
-        User user = new User("Baburao", "baburao@example.com");
-
-        assertEquals("Baburao", user.getName());
-        assertEquals("baburao@example.com", user.getEmailId());
-    }
 
     @Test
-    public void testUserWithEmptyName() {
+    public void shouldThrowExceptionForEmptyOrNullName() {
         assertThrows(IllegalArgumentException.class, () -> new User("", "baburao@example.com"));
-    }
-
-    @Test
-    public void testUserWithNullName() {
         assertThrows(IllegalArgumentException.class, () -> new User(null, "baburao@example.com"));
     }
 
     @Test
-    public void testUserWithEmptyEmailId() {
-        assertThrows(IllegalArgumentException.class, () -> new User("Baburao", ""));
+    public void shouldThrowExceptionForInvalidName() {
+        assertThrows(IllegalArgumentException.class, () -> new User("123", "baburao@example.com"));
+        assertThrows(IllegalArgumentException.class, () -> new User("Babu@rao", "baburao@example.com"));
     }
 
     @Test
-    public void testUserWithNullEmailId() {
+    public void shouldThrowExceptionForEmptyOrNullEmail() {
+        assertThrows(IllegalArgumentException.class, () -> new User("Baburao", ""));
         assertThrows(IllegalArgumentException.class, () -> new User("Baburao", null));
+    }
+
+    @Test
+    public void shouldThrowExceptionForInvalidEmailFormat() {
+        assertThrows(IllegalArgumentException.class, () -> new User("Baburao", "invalid_email"));
+        assertThrows(IllegalArgumentException.class, () -> new User("Baburao", "baburao@example"));
+        assertThrows(IllegalArgumentException.class, () -> new User("Baburao", "baburao@example."));
+    }
+
+    @Test
+    public void shouldReturnCorrectName() {
+        User user = new User("Baburao", "baburao@example.com");
+        assertEquals("Baburao", user.getName());
+    }
+
+    @Test
+    public void shouldReturnCorrectEmail() {
+        User user = new User("Baburao", "baburao@example.com");
+        assertEquals("baburao@example.com", user.getEmailId());
     }
 }
